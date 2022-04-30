dcba24c8e7ee9880dc7859da2e5843f1629014d3
Bless Ambele <blessambel1@gmail.com>
2022-04-30 12:57:42 -0930

    create the subscract test

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 
-from .calc import add
+from .calc import add, subtract
 
 
 class CalcTest(TestCase):
     def test_add_number(self):
         """Test that two numbers are added together"""
-        self.assertEqual(add(2, 1), 5)
+        self.assertEqual(add(2, 1), 3)
+
+    def test_subtract_number(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(subtract(8, 2), 6)
