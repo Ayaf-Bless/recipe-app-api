210a0c5f39a30bfc2ac5938c0060d8d7948d3f5a
Bless Ambele <blessambel1@gmail.com>
2022-05-02 19:02:15 -0930

    test and implement add_fieldset

--- app/core/admin.py
+++ app/core/admin.py
@@ -22,5 +22,11 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -30,3 +30,10 @@ class AdminSiteTest(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_create_user_page(self):
+        """test create user page works"""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
