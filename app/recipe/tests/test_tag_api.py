6333b5062d9d2886eb96f73f737137795ea4d3ff
Ayaf-Bless <blessambel1@gmail.com>
2022-05-24 18:47:26 -0900

    add create tag feature

--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -65,3 +65,21 @@ class PublicTagApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], tag.name)
+
+    def test_create_tag_successfully(self):
+        """Test create Tag successfully"""
+        payload = {"name": "test tag"}
+        self.client.post(TAG_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload["name"]
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test create tag with invalid data"""
+        payload = {"name": ""}
+        res = self.client.post(TAG_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,7 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewsSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewsSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -16,3 +16,9 @@ class TagViewsSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """return objects the current authed user only"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    # def perform_create(self, request, *args, **kwargs):
+    #     """create tags"""
+    def perform_create(self, serializer):
+        """create tags and assign it to a user"""
+        serializer.save(user=self.request.user)
